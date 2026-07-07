30bddc6da1be86c5f02eaf4b898726013e50cc38
Ruben <ruben@example.com>
2026-07-07 10:11:54 -0600

    cambio plansuscripcion y plan basico

--- src/main/java/org/example/PlanBasico.java
+++ src/main/java/org/example/PlanBasico.java
@@ -1,4 +1,17 @@
 package org.example;
 
-public class PlanBasico {
+public class PlanBasico implements {
+    @Override
+    public double calcularCosto(int meses){
+        return meses * 5.00;
+
+        @Override
+                public String getNombrePlan (){
+            return "plan basico";
+            @Override
+                    public String getDescripcion (){
+                return
+            }
+        }
+    }
 }
